1d792eb3af00d647e6ab004b26fecfe4f645ffd9
qiu <qiujiaxp@163.com>
2024-01-03 15:20:36 +0800

    feature: 更新组件版本

--- src/assets/componentLibrary/demand-import.js
+++ src/assets/componentLibrary/demand-import.js
@@ -6,7 +6,7 @@ import {
     bkPopover, bkProcess, bkProgress, bkRadio, bkRadioGroup, bkSearchSelect, bkSelect, bkTableSettingContent,
     bkSideslider, bkSlider, bkSteps, bkSwitcher, bkTab, bkTabPanel, bkTable, bkTableColumn, bkTagInput, bkTimePicker,
     bkTimeline, bkClickoutside, bkTooltips, bkOverflowTips, bkCascade, bkIcon, bkDivider, bkPopconfirm, bkRadioButton, bkTag, bkBigTree, bkAnimateNumber,
-    bkComposeFormItem, bkResizeLayout, bkBadge, bkRoundProgress, bkSpin, bkVirtualScroll, bkTransition, bkColorPicker, bkDescription, bkDescriptionItem,
+    bkComposeFormItem, bkResizeLayout, bkBadge, bkRoundProgress, bkSpin, bkVirtualScroll, bkTransition, bkColorPicker,
     bkDiff, bkTransfer
 } from '@canway/cw-magic-vue'
 
@@ -83,8 +83,8 @@ Vue.use(bkTooltips)
 Vue.use(bkLoading)
 Vue.use(bkOverflowTips)
 Vue.use(bkDiff)
-Vue.use(bkDescription)
-Vue.use(bkDescriptionItem)
+// Vue.use(bkDescription)
+// Vue.use(bkDescriptionItem)
 
 export const $error = (message, delay = 3000) => {
     if (message === 'cancelRequest') {
